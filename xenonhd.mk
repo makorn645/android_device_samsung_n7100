1f0c7b57c8584ff6d8c2582145bbc7d857200a01
Makornthawat Emery <makornthawateugeneemery@gmail.com>
2017-03-08 19:13:53 +0700

    Add a maintainer

--- xenonhd.mk
+++ xenonhd.mk
@@ -44,6 +44,9 @@ PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-N7100
 PRODUCT_MANUFACTURER := samsung
 
+# Device Maintainer
+PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer="makorn645"
+
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t03gxx TARGET_DEVICE=t03g BUILD_FINGERPRINT="samsung/t03gxx/t03g:4.3/JSS15J/N7100XXUEMK4:user/release-keys" PRIVATE_BUILD_DESC="t03gxx-user 4.3 JSS15J N7100XXUEMK4 release-keys"
 
