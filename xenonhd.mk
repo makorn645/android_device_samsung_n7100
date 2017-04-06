6687e51dcfddeffae7f05b07f6887823a1307ab5
Makornthawat Emery <makornthawateugeneemery@gmail.com>
2017-04-06 11:19:03 +0200

    n7100: Use Modified Kernel Adiutor
    Now we use a modified kerneladiutor to support our touchwake feature.
    
    Change-Id: Iffe760ad34b56b2f21d684ee06e13630f937b969

--- xenonhd.mk
+++ xenonhd.mk
@@ -50,6 +50,9 @@ PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer="makorn645"
 # Use magisk root
 ROOT_METHOD=magisk
 
+# We use custom KernelAdiutor
+KERNEL_ADIUTOR=false
+
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t03gxx TARGET_DEVICE=t03g BUILD_FINGERPRINT="samsung/t03gxx/t03g:4.3/JSS15J/N7100XXUEMK4:user/release-keys" PRIVATE_BUILD_DESC="t03gxx-user 4.3 JSS15J N7100XXUEMK4 release-keys"
 
